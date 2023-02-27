3060585c40808f00943e605beff5c06304a64a11
Aleksey Chebykin <a.chebykin@vk.team>
2023-02-28 00:28:33 +0300

    VKR-5: add structure and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import {Routes, Route} from 'react-router-dom';
+import {Header} from "./core/Header/Header";
+
+import './styles/index.scss';
 
 function App() {
-  return (
-    <div>
-    </div>
-  );
+    return (
+        <div className="wrapper">
+            <Header/>
+            <main className="container">
+                <Routes>
+                    <Route path={'/login'} element={<div>auth</div>}/>
+                    <Route path={'/tasks'} element={<div>tasks</div>}/>
+                </Routes>
+            </main>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/core/Header/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import s from './Header.module.scss';
+
+export const Header = () => {
+    return (
+        <div className={s.header}>
+            It's header
+        </div>
+    );
+};
