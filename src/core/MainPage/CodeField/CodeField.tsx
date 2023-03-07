ba1f71e284e74175e45d4bfc0177fb6502adf166
Aleksey Chebykin <a.chebykin@vk.team>
2023-03-08 01:04:15 +0300

    add code editor

--- src/core/MainPage/CodeField/CodeField.tsx
+++ src/core/MainPage/CodeField/CodeField.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import CodeEditor from '@uiw/react-textarea-code-editor';
 import { useTasksStore } from '../../../store/tasksStore';
 
 export const CodeField = () => {
   const currentTask = useTasksStore((state) => state.getCurrentTask());
+  const [code, setCode] = useState('function add(a, b) {\n  return a + b;\n}')
   return (
     <div>
       <h4>{currentTask?.title}</h4>
-      <div>{currentTask?.description}</div>
-      <div>CodeSandBox</div>
+      <p>{currentTask?.description}</p>
+      <div>
+        <CodeEditor
+          value={code}
+          onChange={(env) => setCode(env.target.value)}
+          language='python'
+          placeholder='Please enter your python code'
+          padding={15}
+          minHeight={500}
+          style={{
+            fontSize: 20
+          }}
+        />
+      </div>
     </div>
   );
 };
