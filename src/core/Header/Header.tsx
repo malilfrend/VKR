dd640b45fdd55d2bf5ffe5172af05c809e233b0f
Aleksey Chebykin <a.chebykin@vk.team>
2023-03-11 18:16:34 +0300

    VKR-8: create authStore and make header

--- src/core/Header/Header.tsx
+++ src/core/Header/Header.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import s from './Header.module.scss';
+import { useAuthStore } from '../../store/authStore';
+import { NavLink } from 'react-router-dom';
 
 export const Header = () => {
-    return (
-        <div className={s.header}>
-            It's header
+  const { isAuth, name, lastName, group } = useAuthStore((state) => state);
+  return (
+    <div className={s.wrapper}>
+      <div className={s.container}>
+        <h4>Кафедра ЭПУ</h4>
+        <div>
+          {
+            isAuth ?
+              <div className={s.userInfo}>
+                <p>{name} {lastName}</p>
+                <p>{group}</p>
+              </div>
+              :
+              <NavLink to={'/login'}>Войти</NavLink>
+          }
         </div>
-    );
+      </div>
+    </div>
+  );
 };
---
+++ src/store/authStore.ts
@@ -0,0 +1,14 @@
+import { create } from 'zustand';
+
+interface IAuthStore {
+  isAuth: boolean;
+  name: string;
+  lastName: string;
+  group: number;
+}
+export const useAuthStore = create<IAuthStore>((set, get) => ({
+  isAuth: false,
+  name: 'student',
+  lastName: 'studentov',
+  group: 9202,
+}))
