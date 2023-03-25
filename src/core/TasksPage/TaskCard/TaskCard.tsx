7def4b4e841d36d7772a0391542d48bfda618278
Aleksey Chebykin <a.chebykin@vk.team>
2023-03-25 13:16:45 +0300

    change h4 to h3 in TackCard

--- src/core/TasksPage/TaskCard/TaskCard.tsx
+++ src/core/TasksPage/TaskCard/TaskCard.tsx
@@ -7,7 +7,7 @@ export const TaskCard: React.FC<TTask & { setCurrentTask: (id: number) => void }
   ({ setCurrentTask, title, description, id }) => {
     return (
       <NavLink to={`/task/${id}`} className={s.wrapper} onClick={() => setCurrentTask(id)}>
-        <h4>{title}</h4>
+        <h3>{title}</h3>
         <p>{description}</p>
       </NavLink>
     );
