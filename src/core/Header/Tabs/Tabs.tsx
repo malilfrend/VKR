6cdff04fa52fb902970229c7b5c212ee2a446d7b
Aleksey Chebykin <a.chebykin@vk.team>
2023-04-19 21:01:43 +0300

    fix error

--- src/core/Header/Tabs/Tabs.tsx
+++ src/core/Header/Tabs/Tabs.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 import s from './Tabs.module.scss';
-import { tabs } from './tabsArr';
+import { tabsArr } from './tabsArr';
 
 export const Tabs = () => (
   <div className={s.tabs}>
-    {tabs.map((tab) => (
+    {tabsArr.map((tab) => (
       <NavLink to={`/${tab.url}`} key={tab.name} className={({ isActive }) => (isActive ? `${s.active}` : '')}>
         {tab.name}
       </NavLink>
--- src/core/Header/Tabs/tabsArr.ts
+++ src/core/Header/Tabs/tabsArr.ts
@@ -1,4 +1,4 @@
-export const tabs = [
+export const tabsArr = [
   {
     name: 'Главная',
     url: '',
