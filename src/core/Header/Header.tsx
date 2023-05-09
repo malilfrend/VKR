1f577dc02fefe69f3deb4ea0bd7f709e103aee5a
Aleksey Chebykin <a.chebykin@vk.team>
2023-05-09 23:34:31 +0300

    VKR-18: change color theme

--- src/core/Header/Header.tsx
+++ src/core/Header/Header.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import s from './Header.module.scss';
-import { Tabs } from './Tabs/Tabs';
+import { TabsComponent } from './Tabs/Tabs';
 import { AuthBtn } from './AuthBtn/AuthBtn';
 
 export const Header = () => (
   <div className={s.wrapper}>
     <div className={s.container}>
       <h4>Кафедра ЭПУ</h4>
-      <Tabs />
+      <TabsComponent />
       <AuthBtn />
     </div>
   </div>
--- src/core/Header/Tabs/Tabs.tsx
+++ src/core/Header/Tabs/Tabs.tsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 import s from './Tabs.module.scss';
 import { tabsArr } from './tabsArr';
 
-export const Tabs = () => (
+export const TabsComponent = () => (
   <div className={s.tabs}>
     {tabsArr.map((tab) => (
       <NavLink to={`/${tab.url}`} key={tab.name} className={({ isActive }) => (isActive ? `${s.active}` : '')}>
