b51db5c672ec4c314cd50263e24644b035f1db07
Aleksey Chebykin <a.chebykin@vk.team>
2023-05-19 22:21:23 +0300

    VKR-26: change logic of work with indicators

--- src/core/Indicators/Indicators.tsx
+++ src/core/Indicators/Indicators.tsx
@@ -3,25 +3,25 @@ import { Button } from '@mui/joy';
 import s from './Indicators.module.scss';
 
 const indicators = [1, 2, 3, 4, 5, 6, 7];
-const indicatorsOrder = [2, 4, 1, 3, 5, 7, 6];
+
+const indicatorsMap = new Map([
+  [1, true],
+  [2, true],
+  [3, true],
+  [4, true],
+  [5, true],
+  [6, true],
+  [7, false],
+]);
 
 export const Indicators = () => {
-  const [currentIndicator, setCurrentIndicator] = useState(0);
-  // eslint-disable-next-line consistent-return
-  const setNewLed = (order: number[], i = 0) => {
-    if (order.length === i) {
-      return null;
-    }
-    setCurrentIndicator(order[i]);
-    setTimeout(() => {
-      setNewLed(order, i + 1);
-    }, 1000);
-  };
+  const [play, setPlay] = useState(false);
+
   return (
     <div className={s.wrapper}>
       <div className={s.indicatorsGroup}>
         {indicators.map((indicator) => (
-          <div className={`${s.indicator} ${currentIndicator === indicator ? s.active : ''}`} key={indicator}>
+          <div className={`${s.indicator} ${indicatorsMap.get(indicator) && play ? s.active : ''}`} key={indicator}>
             {indicator}
           </div>
         ))}
@@ -32,7 +32,7 @@ export const Indicators = () => {
         type="button"
         color="success"
         onClick={() => {
-          setNewLed(indicatorsOrder);
+          setPlay(!play);
         }}
       >
         Пуск
